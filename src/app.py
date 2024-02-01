a8642cde4655a3408db274a4ca074e443ff02aed
kabina <kabina@nheo.duckdns.org>
2024-02-01 14:46:52 +0900

    commit8

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from confluent_kafka import Consumer, Producer
 from csms_backend import retr_charger_list
 
-# Commit7
+# Commit8
 app = Flask(__name__)
 app.config['API_KEY'] = os.environ.get('API_KEY')
 
