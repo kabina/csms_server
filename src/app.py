49803d177d0ae7bd0d05dfb50df4eb15c8736898
kabina <kabina@nheo.duckdns.org>
2024-02-01 09:21:39 +0900

    TEST

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from confluent_kafka import Consumer, Producer
 from csms_backend import retr_charger_list
 
+# TEST
 app = Flask(__name__)
 app.config['API_KEY'] = os.environ.get('API_KEY')
 
