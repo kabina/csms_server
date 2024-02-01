25818e1c5720308e824f09c0cbf5421cd8307e67
kabina <kabina@nheo.duckdns.org>
2024-02-01 14:45:22 +0900

    add runapp, stop_gunicorn.sh

---
+++ runapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export API_KEY=fmvGcA0N4w3IFHwnr56ba2hC1Ef9Is451JyF6LOD
+export DB_USER=csms
+export DB_PASSWORD=1q2w3e4r
+export DB_HOST=juha.iptime.org
+export DB_PORT=5432
+export DB_SEVER=csms
+
+cd /var/gitlab-runner/prd/csms-server/src
+/home/kabina/envs/csms/bin/gunicorn -w 4 -b 0.0.0.0:5000 ev_rest:app
