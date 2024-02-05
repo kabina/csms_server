85345b726b799eab3d3b6496502325ec8aeb3aad
안창선 <nheo.an@gmail.com>
2024-02-05 17:02:26 +0900

    502

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from confluent_kafka import Consumer, Producer
 from csms_backend import retr_charger_list
 
-# chmod test erase appuser test test 10
+# chmod test erase appuser test test 11
 app = Flask(__name__)
 app.config['API_KEY'] = os.environ.get('API_KEY')
 
