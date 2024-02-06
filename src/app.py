3fe0b8a0b2a182fb31d6f0a9a91f6b5b5e4740ac
안창선 <nheo.an@gmail.com>
2024-02-06 10:49:32 +0900

    2024. 02. 06. (화) 10:49:32 KST

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from confluent_kafka import Consumer, Producer
 from csms_backend import retr_charger_list
 
-# two port final?????
+# two port final??????
 app = Flask(__name__)
 app.config['API_KEY'] = os.environ.get('API_KEY')
 
