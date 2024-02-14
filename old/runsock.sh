08f37b6e83f741d8ec7c949c70ab06d0c44d42d1
안창선 <nheo.an@gmail.com>
2024-02-14 17:34:28 +0900

    commit of 2024. 02. 14. (수) 17:34:28 KST

---
+++ old/runsock.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export SOCK_PORT=8765
+export DB_USER=csms
+export DB_PASSWORD=1q2w3e4r
+export DB_HOST=juha.iptime.org
+export DB_PORT=5432
+export DB_SEVER=csms
+
+/home/kabina/envs/csms/bin/python src/csms_server.py
