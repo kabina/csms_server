fe432676f43326b579a7e6434462fc25a366814b
안창선 <nheo.an@gmail.com>
2024-02-19 12:39:32 +0900

    commit of 2024. 02. 19. (월) 12:39:32 KST

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker stop `docker ps | grep runapp | awk '{print $1}'`
 docker rm `docker ps -a| grep runapp | awk '{print $1}'`
-docker run -d -p 5000:5000 kabina/csms-server:latest
+docker run -d -p 5000:5000 kabina/csms-server:latest -v /config:/var/gitlab-runner/config
 docker stop `docker ps | grep runsock | awk '{print $1}'`
 docker rm `docker ps -a| grep runsock | awk '{print $1}'`
 docker run -d -p 8765:8765 kabina/socket-server:latest
