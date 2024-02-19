607086e601582edf4152bc6776c7ac436be51ba5
안창선 <nheo.an@gmail.com>
2024-02-19 13:11:50 +0900

    commit of 2024. 02. 19. (월) 13:11:50 KST

--- src/runapp.sh
+++ src/runapp.sh
@@ -6,4 +6,5 @@ export DB_HOST=nheo.duckdns.org
 export DB_PORT=5432
 export DB_SEVER=csms
 
-gunicorn --certfile=/config/nheo.duckdns.org.crt --keyfile=/config/nheo.duckdns.org.key -w 4 -b 0.0.0.0:5000 ev_rest:app
+#gunicorn --certfile=/config/nheo.duckdns.org.crt --keyfile=/config/nheo.duckdns.org.key -w 4 -b 0.0.0.0:5000 ev_rest:app
+gunicorn -w 4 -b 0.0.0.0:5000 ev_rest:app
