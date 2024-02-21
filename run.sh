c26682c74707cb3821deff9d4d51c154d0dbf003
안창선 <nheo.an@gmail.com>
2024-02-21 14:15:16 +0900

    commit of 2024. 02. 21. (수) 14:15:16 KST

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker ps | grep csms-server | awk '{print $1}' | xargs -r docker stop
 docker ps -a | grep csms-server | awk '{print $1}' | xargs -r docker rm
 docker images | grep csms-server | awk '{print $3}' | xargs -r docker rmi
-docker run -d --name "csms-server" -v /var/gitlab/config/ssl:/config -p 5000:80 kabina/csms-server:latest
+docker run -d --name "csms-server" -v /var/gitlab/config/ssl:/config -p 5000:443 kabina/csms-server:latest
 docker ps | grep socket-server | awk '{print $1}' | xargs -r docker stop
 docker ps -a | grep socket-server | awk '{print $1}' | xargs -r docker rm
 docker images | grep socket-server | awk '{print $3}' | xargs -r docker rmi
--- src/app.py
+++ src/app.py
@@ -98,3 +98,4 @@ if __name__ == '__main__':
 # deploy commit name : commit of 2024. 02. 21. (수) 13:51:30 KST\n
 # deploy commit name : commit of 2024. 02. 21. (수) 13:52:09 KST\n
 # deploy commit name : commit of 2024. 02. 21. (수) 14:00:39 KST\n
+# deploy commit name : commit of 2024. 02. 21. (수) 14:15:16 KST\n
