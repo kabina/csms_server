dcfae9112cd1b731abba9d46f1721e2572057658
안창선 <nheo.an@gmail.com>
2024-02-21 14:34:49 +0900

    commit of 2024. 02. 21. (수) 14:34:49 KST

--- src/app.py
+++ src/app.py
@@ -101,3 +101,4 @@ if __name__ == '__main__':
 # deploy commit name : commit of 2024. 02. 21. (수) 14:15:16 KST\n
 # deploy commit name : commit of 2024. 02. 21. (수) 14:23:40 KST\n
 # deploy commit name : commit of 2024. 02. 21. (수) 14:27:36 KST\n
+# deploy commit name : commit of 2024. 02. 21. (수) 14:34:49 KST\n
