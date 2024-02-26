46f87ed5e873f5e3c6051f2157bfcb963005b496
ChangSun An <nheo.an@gmail.com>
2024-02-26 13:54:08 +0900

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-export API_KEY=fmvGcA0N4w3IFHwnr56ba2hC1Ef9Is451JyF6LOD
-export DB_USER=csms
-export DB_PASSWORD=1q2w3e4r
-export DB_HOST=juha.iptime.org
-export DB_PORT=5432
-export DB_SEVER=csms
 
 /home/kabina/venv/csms/bin/gunicorn -w 4 -b 0.0.0.0:5000 ev_rest:app
