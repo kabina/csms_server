2fdfa5d1c3560dcf65738a734b870ffdd76d5173
ChangSun An <nheo.an@gmail.com>
2024-02-26 13:54:30 +0900

    Update runsock.sh

--- runsock.sh
+++ runsock.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-export SOCK_SERVER=nheo.duckdns.org
-export SOCK_PORT=8765
-export DB_USER=csms
-export DB_PASSWORD=1q2w3e4r
-export DB_HOST=juha.iptime.org
-export DB_PORT=5432
-export DB_SEVER=csms
 
 /home/kabina/venv/csms/bin/python csms_server.py
